977fc70567f43415e9ce6f1470fbf4bf7f6a9f58
ShivaKumarKalvaTR <shivakumar.kalva@thomsonreuters.com>
2020-10-02 09:36:29 +0530

    Revert " added hello and list files"
    This reverts commit cc9fd0d8

---
+++ pythonBackend/hello.py
@@ -0,0 +1,10 @@
+print("hello first demo")
+
+x = 3
+print(x)
+
+print(type(3))
+
+name = "shiva"
+
+print("{} {}".format("My Name is", name))
---
+++ pythonBackend/second.py
@@ -0,0 +1,21 @@
+# In python we can give the heterogenoud data types
+
+a = [1, 2, "shiva", "hanuman", 2, 1]
+print(a)
+print(a[0])
+print(a[-1])
+print(a[1:3])
+a.insert(6, "shi")
+print(a)
+
+###########  ADDING THE LETTERS ######################
+a.append("shivaji")
+print(a)
+
+###########  ASSIGINING THE LETTERS ######################
+a[2] = "jai"
+print(a)
+
+################### DELTER
+del a[0]
+print(a)
