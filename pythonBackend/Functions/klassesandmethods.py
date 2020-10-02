c4df114a144ad3a634a5682f644ee4d421a62e46
ShivaKumarKalvaTR <shivakumar.kalva@thomsonreuters.com>
2020-10-02 23:48:46 +0530

    added functions related programs

---
+++ pythonBackend/Functions/klassesandmethods.py
@@ -0,0 +1,10 @@
+class calculator:
+    num = 1000
+
+    def getdata(self):
+        print(" the value is")
+
+
+obj = calculator()
+print(obj.getdata())
+print(obj.num)
---
+++ pythonBackend/Functions/konstructor.py
@@ -0,0 +1,22 @@
+class konstructor:
+    vars = 2  # class variables they are equal
+
+    ###########Even thought we have not called the constructor, it was called explictly ###########
+
+    def __init__(self, a, b):
+        self.firstnumber = a  # instance variables they keep on changing for every object crreation
+        self.secondnumber = b  # instance variables they keep on changing for every object crreation
+
+    def getdata(self):
+        print("i am getting the data")
+
+    def summation(self):
+        return self.firstnumber + self.secondnumber
+
+
+x = konstructor(1, 2)
+print(x.summation())
+x1 = konstructor(3, 4)
+print(x1.summation())
+
+print(x.getdata())
