fadb6e00d1b4737515b3cf26c95ee3bb67e20d67
ShivaKumarKalvaTR <shivakumar.kalva@thomsonreuters.com>
2020-10-04 22:09:40 +0530

    json changes

---
+++ pythonBackend/Files/json/runtimedata.py
@@ -0,0 +1,12 @@
+import json
+
+with open("C:\\MySpace\\COMPLETED\\datas.json") as p:
+    content = json.load(p)
+    for check in content['checks']:
+        if check['streetAddress'] == '127':
+            print(check['city'])
+            assert check['city'] == 'San Jone'
+
+with open("C:\\MySpace\\COMPLETED\\data.json") as f:
+    cont = json.load(f)
+    assert content == cont
