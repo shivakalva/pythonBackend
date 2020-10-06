49dc19c6e4bd87fcb243f138254a1f404c8a4a0a
ShivaKumarKalvaTR <shivakumar.kalva@thomsonreuters.com>
2020-10-06 20:09:16 +0530

    Post and Delete changes

---
+++ pythonBackend/API Testing/payload.py
@@ -0,0 +1,10 @@
+def addpayload(isbn):
+    body = {
+
+        "name": "Learn Appium Automation with Java",
+        "isbn": "hli",
+        "aisle": "124",
+        "author": "John foe"
+    }
+
+    return body
